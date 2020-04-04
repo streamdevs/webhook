8fc762f08e6c13770716837b97c3421a745f4938
Orestes Carracedo <orestes@orestes.io>
2020-04-04 19:00:19 +0200

    feat(server): Start server using config

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+import { Server } from '@hapi/hapi'
+
 // TODO: Validate arguments before starting
+
 
 const config = {
     "STREAMLABS_TOKEN": process.env['STREAMLABS_TOKEN'],
     "port": process.env['PORT'] || process.env['HTTP_PORT'] || 8080
 };
 
+export const initServer = (config) => {
+    const server = new Server({
+        port: config.port
+    });
+
+    return server;
+}
+
+initServer(config);
