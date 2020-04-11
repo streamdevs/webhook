1d75037bb316ef2a2dd6708cd955063f6ccfb6cf
Santiago Martín Agra <santiagomartinagra@gmail.com>
2020-04-11 19:21:08 +0200

    chore: apply eslint and prettier rules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const { initServer } = require("./server");
+const { initServer } = require('./server');
 
 const config = {
-  STREAMLABS_TOKEN: process.env["STREAMLABS_TOKEN"],
-  port: process.env["PORT"] || process.env["HTTP_PORT"] || 8080,
+	STREAMLABS_TOKEN: process.env['STREAMLABS_TOKEN'],
+	port: process.env['PORT'] || process.env['HTTP_PORT'] || 8080,
 };
 
 initServer(config).start();
--- src/server.js
+++ src/server.js
@@ -1,84 +1,84 @@
-const { Server } = require("@hapi/hapi");
-const joi = require("@hapi/joi");
-const axios = require("axios");
+const { Server } = require('@hapi/hapi');
+const joi = require('@hapi/joi');
+const axios = require('axios');
 
 // TODO: Validate arguments before starting
 
-const STREAMLABS_ENDPOINT = "https://streamlabs.com/api/v1.0/alerts";
+const STREAMLABS_ENDPOINT = 'https://streamlabs.com/api/v1.0/alerts';
 
 const initServer = (config) => {
-  const server = new Server({
-    port: config.port,
-  });
+	const server = new Server({
+		port: config.port,
+	});
 
-  server.route([
-    {
-      method: "POST",
-      path: "/github",
-      options: {
-        validate: {
-          headers: joi
-            .object({ "x-github-event": joi.string().required() })
-            .unknown(),
-          payload: joi
-            .object({
-              action: joi.string(),
-              hook: joi
-                .object({ events: joi.array().items(joi.string()) })
-                .unknown(),
-              sender: joi
-                .object({ login: joi.string().required() })
-                .required()
-                .unknown(),
-              repository: joi
-                .object({ full_name: joi.string().required() })
-                .required()
-                .unknown(),
-            })
-            .unknown(),
-        },
-      },
-      handler: async (request, h) => {
-        const { payload, headers } = request;
-        const event = headers["x-github-event"];
-        const {
-          repository: { full_name: repositoryFullName },
-        } = payload;
+	server.route([
+		{
+			method: 'POST',
+			path: '/github',
+			options: {
+				validate: {
+					headers: joi
+						.object({ 'x-github-event': joi.string().required() })
+						.unknown(),
+					payload: joi
+						.object({
+							action: joi.string(),
+							hook: joi
+								.object({ events: joi.array().items(joi.string()) })
+								.unknown(),
+							sender: joi
+								.object({ login: joi.string().required() })
+								.required()
+								.unknown(),
+							repository: joi
+								.object({ full_name: joi.string().required() })
+								.required()
+								.unknown(),
+						})
+						.unknown(),
+				},
+			},
+			handler: async (request, h) => {
+				const { payload, headers } = request;
+				const event = headers['x-github-event'];
+				const {
+					repository: { full_name: repositoryFullName },
+				} = payload;
 
-        if (event === "ping" && request.payload.hook.events.includes("star")) {
-          await axios.post(STREAMLABS_ENDPOINT, {
-            access_token: config.STREAMLABS_TOKEN,
-            type: "follow",
-            message: `🎉 Your repo *${repositoryFullName}* is configured correctly for *star* events 🎉`,
-          });
+				if (event === 'ping' && request.payload.hook.events.includes('star')) {
+					await axios.post(STREAMLABS_ENDPOINT, {
+						access_token: config.STREAMLABS_TOKEN,
+						type: 'follow',
+						message: `🎉 Your repo *${repositoryFullName}* is configured correctly for *star* events 🎉`,
+					});
 
-          return h.response().code(200);
-        }
+					return h.response().code(200);
+				}
 
-        if (event === "star" && request.payload.action === "created") {
-          const {
-            sender: { login: senderLogin },
-          } = payload;
+				if (event === 'star' && request.payload.action === 'created') {
+					const {
+						sender: { login: senderLogin },
+					} = payload;
 
-          await axios.post(STREAMLABS_ENDPOINT, {
-            access_token: config.STREAMLABS_TOKEN,
-            type: "follow",
-            message: `*${senderLogin}* just starred *${repositoryFullName}*`,
-          });
+					await axios.post(STREAMLABS_ENDPOINT, {
+						access_token: config.STREAMLABS_TOKEN,
+						type: 'follow',
+						message: `*${senderLogin}* just starred *${repositoryFullName}*`,
+					});
 
-          return h.response().code(200);
-        }
+					return h.response().code(200);
+				}
 
-        return h.response({
-          message: `Ignoring event: '${event}'`,
-        });
-      },
-    },
-  ]);
+				return h.response({
+					message: `Ignoring event: '${event}'`,
+				});
+			},
+		},
+	]);
 
-  return server;
+	return server;
 };
 
 module.exports = {
-  initServer,
+	initServer,
 };
