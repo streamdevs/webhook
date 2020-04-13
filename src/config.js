40d83640a32fbf836316f15a5b5275855fc301f1
Santiago Martín Agra <santiagomartinagra@gmail.com>
2020-04-13 21:44:05 +0200

    feat: load Twitch config from environment

--- src/config.js
+++ src/config.js
@@ -4,6 +4,9 @@ const getConfig = () => {
 			logging: false,
 			STREAMLABS_TOKEN: 'token',
 			STREAMLABS_ENDPOINT: 'https://streamlabs.com/api/v1.0/alerts', // TODO: remove
+			TWITCH_BOT_NAME: '',
+			TWITCH_BOT_TOKEN: '',
+			TWITCH_BOT_CHANEL: '',
 			port: 8080,
 		};
 	}
@@ -13,6 +16,9 @@ const getConfig = () => {
 		logging: true,
 		STREAMLABS_TOKEN: process.env['STREAMLABS_TOKEN'],
 		STREAMLABS_ENDPOINT: 'https://streamlabs.com/api/v1.0/alerts', // TODO: remove
+		TWITCH_BOT_NAME: process.env['TWITCH_BOT_NAME'],
+		TWITCH_BOT_TOKEN: process.env['TWITCH_BOT_TOKEN'],
+		TWITCH_BOT_CHANEL: process.env['TWITCH_BOT_CHANEL'],
 		port: process.env['PORT'] || process.env['HTTP_PORT'] || 8080,
 	};
 };
