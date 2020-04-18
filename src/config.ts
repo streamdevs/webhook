f638bc9198ae409d2b09d6e22e2acb85d040fdd8
Santiago Martín Agra <santiagomartinagra@gmail.com>
2020-04-18 20:49:20 +0200

    refactor: migrate 'config' to TypeScript

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,17 @@
-const getConfig = () => {
+export interface Config {
+	logging: boolean;
+	STREAMLABS_TOKEN?: string;
+	TWITCH_BOT_NAME?: string;
+	TWITCH_BOT_TOKEN?: string;
+	TWITCH_BOT_CHANNEL?: string;
+	port: number | string;
+}
+
+export const getConfig = (): Config => {
 	if (process.env.NODE_ENV === 'test') {
 		return {
 			logging: false,
 			STREAMLABS_TOKEN: 'token',
-			STREAMLABS_ENDPOINT: 'https://streamlabs.com/api/v1.0/alerts', // TODO: remove
 			TWITCH_BOT_NAME: '',
 			TWITCH_BOT_TOKEN: '',
 			TWITCH_BOT_CHANNEL: '',
@@ -11,18 +19,12 @@ const getConfig = () => {
 		};
 	}
 
-	// TODO: Validate arguments before starting
 	return {
 		logging: true,
 		STREAMLABS_TOKEN: process.env['STREAMLABS_TOKEN'],
-		STREAMLABS_ENDPOINT: 'https://streamlabs.com/api/v1.0/alerts', // TODO: remove
 		TWITCH_BOT_NAME: process.env['TWITCH_BOT_NAME'],
 		TWITCH_BOT_TOKEN: process.env['TWITCH_BOT_TOKEN'],
 		TWITCH_BOT_CHANNEL: process.env['TWITCH_BOT_CHANNEL'],
 		port: process.env['PORT'] || process.env['HTTP_PORT'] || 8080,
 	};
-};
-
-module.exports = {
-	getConfig,
 };
