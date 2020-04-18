033bb62faa5ad4076a096cb3ce335a4458cb70ce
Santiago Martín Agra <santiagomartinagra@gmail.com>
2020-04-18 22:23:41 +0200

    refactor: migrate 'githubWebhookHeaders' schema to TypeScript

--- src/schemas/gitHubWebhookHeaders.js
+++
@@ -1,5 +0,0 @@
-const joi = require('@hapi/joi');
-function gitHubWebhookHeaders() {
-	return joi.object({ 'x-github-event': joi.string().required() }).unknown();
-}
-exports.gitHubWebhookHeaders = gitHubWebhookHeaders;
