a99fa6f5dd8ae63f89a292bdb0f8c9df253044a8
Santiago Martín Agra <santiagomartinagra@gmail.com>
2020-04-19 22:35:20 +0200

    refactor: 'PullRequestOpened' to extend 'Reaction'

--- src/reactions/github/pull-request-opened.ts
+++ src/reactions/github/pull-request-opened.ts
@@ -1,63 +1,10 @@
-import { TwitchChat } from '../../services/TwitchChat';
-import { StreamLabs } from '../../services/StreamLabs';
+import { Reaction, ReactionHandleOptions } from './reaction';
 
-interface HandleOptions {
-	payload: any;
-}
-
-export class PullRequestOpened {
-	public constructor(
-		private twitchChat: TwitchChat,
-		private streamlabs: StreamLabs,
-	) {}
-
-	private async notifyTwitch({ payload }: HandleOptions) {
-		try {
-			const message = `*${payload.pull_request.user.login}* just opened a pull request in ${payload.repository.html_url}`;
-			await this.twitchChat.send(message);
-
-			return {
-				notified: true,
-				message,
-			};
-		} catch {
-			// TODO: add logging
-
-			return {
-				notified: false,
-				message: '',
-			};
-		}
+export class PullRequestOpened extends Reaction {
+	getStreamLabsMessage({ payload }: ReactionHandleOptions): string {
+		return `*${payload.pull_request.user.login}* just opened a pull request in *${payload.repository.full_name}*`;
 	}
-
-	public async notifyStreamLabs({ payload }: HandleOptions) {
-		try {
-			const message = `*${payload.pull_request.user.login}* just opened a pull request in *${payload.repository.full_name}*`;
-			await this.streamlabs.alert({ message });
-
-			return {
-				notified: true,
-				message,
-			};
-		} catch {
-			// TODO: add logging
-
-			return {
-				notified: false,
-				message: '',
-			};
-		}
-	}
-
-	public async handle({ payload }: HandleOptions) {
-		const [twitchChat, streamlabs] = await Promise.all([
-			this.notifyTwitch({ payload }),
-			this.notifyStreamLabs({ payload }),
-		]);
-
-		return {
-			twitchChat,
-			streamlabs,
-		};
+	getTwitchChatMessage({ payload }: ReactionHandleOptions): string {
+		return `*${payload.pull_request.user.login}* just opened a pull request in ${payload.repository.html_url}`;
 	}
 }
