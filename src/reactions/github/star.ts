8079b7fec13bdc2c41fa6ab1911b0ec0cbb5d0be
Santiago Martín Agra <santiagomartinagra@gmail.com>
2020-04-19 22:53:43 +0200

    chore: add 'canHandle' method to 'Star'

--- src/reactions/github/star.ts
+++ src/reactions/github/star.ts
@@ -1,6 +1,14 @@
-import { Reaction, ReactionHandleOptions } from './reaction';
+import {
+	Reaction,
+	ReactionCanHandleOptions,
+	ReactionHandleOptions,
+} from './reaction';
 
 export class Star extends Reaction {
+	canHandle({ payload, event }: ReactionCanHandleOptions): boolean {
+		return event === 'star' && payload.action === 'created';
+	}
+
 	getStreamLabsMessage({ payload }: ReactionHandleOptions): string {
 		return `*${payload.sender.login}* just starred *${payload.repository.full_name}*`;
 	}
