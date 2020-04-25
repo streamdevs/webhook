502a57c9c33ac24e4be1efeac0768bccca1d14af
Orestes Carracedo <orestes@orestes.io>
2020-04-25 22:51:21 +0200

    feat: Add CheckRun reaction

---
+++ src/reactions/github/check-run.ts
@@ -0,0 +1,50 @@
+import {
+	Reaction,
+	ReactionCanHandleOptions,
+	ReactionHandleOptions,
+} from './reaction';
+import { CheckRunPayload } from '../../schemas/github/check-run-payload';
+
+export class CheckRun extends Reaction<CheckRunPayload> {
+	canHandle({
+		event,
+		payload,
+	}: ReactionCanHandleOptions<CheckRunPayload>): boolean {
+		return event === 'check_run' && payload.check_run.status === 'completed';
+	}
+
+	getStreamLabsMessage({
+		payload,
+	}: ReactionHandleOptions<CheckRunPayload>): string {
+		const conclusion = payload.check_run.conclusion;
+		const repositoryFullName = payload.repository.full_name;
+
+		if (conclusion === 'failure') {
+			return `The build for ${repositoryFullName} just failed 🙃`;
+		}
+
+		if (conclusion === 'success') {
+			return `${repositoryFullName} built successfully ✨!`;
+		}
+
+		throw new Error(`Unhandled check_run conclusion: ${conclusion}`);
+	}
+
+	getTwitchChatMessage({
+		payload,
+	}: ReactionHandleOptions<CheckRunPayload>): string {
+		const conclusion = payload.check_run.conclusion;
+		const repositoryFullName = payload.repository.full_name;
+		const resultUrl = payload.check_run.html_url;
+
+		if (conclusion === 'failure') {
+			return `The build for ${repositoryFullName} just failed 🙃. See ${resultUrl} for details.`;
+		}
+
+		if (conclusion === 'success') {
+			return `${repositoryFullName} built successfully ✨!`;
+		}
+
+		throw new Error(`Unhandled check_run conclusion: ${conclusion}`);
+	}
+}
---
+++ test/reactions/github/check-run.spec.ts
@@ -0,0 +1,179 @@
+import { TwitchChatMock } from '../../__mocks__/TwitchChat';
+import { StreamLabsMock } from '../../__mocks__/StreamLabs';
+import { TwitchChat } from '../../../src/services/TwitchChat';
+import { StreamLabs } from '../../../src/services/StreamLabs';
+
+import { CheckRun } from '../../../src/reactions/github/check-run';
+
+import { CheckRunPayload } from '../../../src/schemas/github/check-run-payload';
+
+describe('CheckRun', async () => {
+	let twitchChat: TwitchChat;
+	let streamLabs: StreamLabs;
+
+	beforeEach(() => {
+		twitchChat = new TwitchChatMock();
+		streamLabs = new StreamLabsMock();
+	});
+	describe('#canHandle', async () => {
+		it('returns true if the action is === completed AND the status is === completed AND conclusion === success', async () => {
+			const subject = new CheckRun(twitchChat, streamLabs);
+			const event = 'check_run';
+			const payload: CheckRunPayload = {
+				action: 'completed',
+				check_run: {
+					html_url: 'https://github.com/Codertocat/Hello-World/runs/128620228',
+					conclusion: 'success',
+					status: 'completed',
+				},
+				repository: {
+					full_name: 'streamdevs/webhook',
+					html_url: 'https://github.com/streamdevs/webhook',
+				},
+				sender: {
+					login: 'orestes',
+				},
+			};
+
+			expect(subject.canHandle({ event, payload })).toEqual(true);
+		});
+		it('returns true if the action is === completed AND the status is === completed AND conclusion === failure', async () => {
+			const subject = new CheckRun(twitchChat, streamLabs);
+			const event = 'check_run';
+			const payload: CheckRunPayload = {
+				action: 'completed',
+				check_run: {
+					html_url: 'https://github.com/Codertocat/Hello-World/runs/128620228',
+					conclusion: 'failure',
+					status: 'completed',
+				},
+				repository: {
+					full_name: 'streamdevs/webhook',
+					html_url: 'https://github.com/streamdevs/webhook',
+				},
+				sender: {
+					login: 'orestes',
+				},
+			};
+
+			expect(subject.canHandle({ event, payload })).toEqual(true);
+		});
+		it('returns false if the action is !== completed', async () => {
+			const subject = new CheckRun(twitchChat, streamLabs);
+			const event = 'check_run';
+			const payload: CheckRunPayload = {
+				action: 'created',
+				check_run: {
+					html_url: 'https://github.com/Codertocat/Hello-World/runs/128620228',
+					conclusion: null,
+					status: 'queued',
+				},
+				repository: {
+					full_name: 'streamdevs/webhook',
+					html_url: 'https://github.com/streamdevs/webhook',
+				},
+				sender: {
+					login: 'orestes',
+				},
+			};
+
+			expect(subject.canHandle({ event, payload })).toEqual(false);
+		});
+	});
+	describe('#getStreamLabsMessage', async () => {
+		it('generates the appropriate message when the build is successful', () => {
+			const subject = new CheckRun(twitchChat, streamLabs);
+			const repositoryFullName = 'streamdevs/webhook';
+			const payload: CheckRunPayload = {
+				action: 'completed',
+				check_run: {
+					html_url: 'https://github.com/Codertocat/Hello-World/runs/128620228',
+					conclusion: 'success',
+					status: 'completed',
+				},
+				repository: {
+					full_name: repositoryFullName,
+					html_url: 'https://github.com/streamdevs/webhook',
+				},
+				sender: {
+					login: 'orestes',
+				},
+			};
+
+			expect(subject.getStreamLabsMessage({ payload })).toEqual(
+				`${repositoryFullName} built successfully ✨!`,
+			);
+		});
+		it('generates the appropriate message when the build fails', () => {
+			const subject = new CheckRun(twitchChat, streamLabs);
+			const repositoryFullName = 'streamdevs/webhook';
+			const payload: CheckRunPayload = {
+				action: 'completed',
+				check_run: {
+					html_url: 'https://github.com/Codertocat/Hello-World/runs/128620228',
+					conclusion: 'failure',
+					status: 'completed',
+				},
+				repository: {
+					full_name: repositoryFullName,
+					html_url: 'https://github.com/streamdevs/webhook',
+				},
+				sender: {
+					login: 'orestes',
+				},
+			};
+
+			expect(subject.getStreamLabsMessage({ payload })).toEqual(
+				`The build for ${repositoryFullName} just failed 🙃`,
+			);
+		});
+	});
+	describe('#getTwitchChatMessage', async () => {
+		it('generates the appropriate message when the build is successful', () => {
+			const subject = new CheckRun(twitchChat, streamLabs);
+			const repositoryFullName = 'streamdevs/webhook';
+			const payload: CheckRunPayload = {
+				action: 'completed',
+				check_run: {
+					html_url: 'https://github.com/Codertocat/Hello-World/runs/128620228',
+					conclusion: 'success',
+					status: 'completed',
+				},
+				repository: {
+					full_name: repositoryFullName,
+					html_url: 'https://github.com/streamdevs/webhook',
+				},
+				sender: {
+					login: 'orestes',
+				},
+			};
+
+			expect(subject.getTwitchChatMessage({ payload })).toEqual(
+				`${repositoryFullName} built successfully ✨!`,
+			);
+		});
+		it('generates the appropriate message when the build fails', () => {
+			const subject = new CheckRun(twitchChat, streamLabs);
+			const repositoryFullName = 'streamdevs/webhook';
+			const payload: CheckRunPayload = {
+				action: 'completed',
+				check_run: {
+					html_url: 'https://github.com/Codertocat/Hello-World/runs/128620228',
+					conclusion: 'failure',
+					status: 'completed',
+				},
+				repository: {
+					full_name: repositoryFullName,
+					html_url: 'https://github.com/streamdevs/webhook',
+				},
+				sender: {
+					login: 'orestes',
+				},
+			};
+
+			expect(subject.getTwitchChatMessage({ payload })).toEqual(
+				`The build for ${repositoryFullName} just failed 🙃. See ${payload.check_run.html_url} for details.`,
+			);
+		});
+	});
+});
