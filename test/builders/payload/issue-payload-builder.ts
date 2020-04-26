1d3bcadd5c39b9c74bb696eab417ed650ecdddbf
Santiago Martín Agra <santiagomartinagra@gmail.com>
2020-04-26 21:37:01 +0200

    refactor: add new 'IssuePayloadBuilder'

---
+++ test/builders/payload/issue-payload-builder.ts
@@ -0,0 +1,25 @@
+import { IssuePayload } from '../../../src/schemas/github/issue-payload';
+import { random, internet } from 'faker';
+import { merge } from 'lodash';
+
+export class IssuePayloadBuilder {
+	private payload: IssuePayload = {
+		action: random.arrayElement(['opened', 'assigned']),
+		repository: {
+			full_name: 'streamdevs/webhook',
+			html_url: 'https://github.com/streamdevs/webhook',
+		},
+		sender: { login: internet.userName() },
+		assignee: { login: internet.userName() },
+	};
+
+	public with(payload: Partial<IssuePayload>): IssuePayloadBuilder {
+		merge(this.payload, payload);
+
+		return this;
+	}
+
+	public getInstance(): IssuePayload {
+		return this.payload;
+	}
+}
