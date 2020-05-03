60219c716d147ff5726ca44a90c56028d8ca27a7
Santiago Martín Agra <santiagomartinagra@gmail.com>
2020-05-03 19:51:45 +0200

    chore: add new param to 'IssuePayload'

--- src/schemas/github/issue-payload.ts
+++ src/schemas/github/issue-payload.ts
@@ -1,6 +1,9 @@
 import { RepositoryWebhookPayload } from './repository-webhook-payload';
 
+type url = string;
+
 export interface IssuePayload extends RepositoryWebhookPayload {
 	action: string;
 	assignee?: { login: string };
+	issue: { html_url: url };
 }
