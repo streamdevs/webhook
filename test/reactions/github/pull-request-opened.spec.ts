a31469457abec3fc04406a76982d7de4d670773e
Santiago Martín Agra <santiagomartinagra@gmail.com>
2020-05-03 19:45:09 +0200

    test: fix pull request payload types

--- test/reactions/github/pull-request-opened.spec.ts
+++ test/reactions/github/pull-request-opened.spec.ts
@@ -3,6 +3,7 @@ import { PullRequestPayload } from '../../../src/schemas/github/pull-request-pay
 import { StreamLabsMock } from '../../__mocks__/StreamLabs';
 import { TwitchChatMock } from '../../__mocks__/TwitchChat';
 import { Config } from '../../../src/config';
+import { PullRequestPayloadBuilder } from '../../builders/github/pull-request-payload-builder';
 
 describe('PullRequestOpened', () => {
 	let streamlabs: StreamLabsMock;
@@ -17,15 +18,17 @@ describe('PullRequestOpened', () => {
 		let payload: PullRequestPayload;
 
 		beforeEach(() => {
-			payload = {
-				action: 'opened',
-				repository: {
-					full_name: 'streamdevs/webhook',
-					html_url: 'https://github.com/streamdevs/webhook',
-				},
-				pull_request: { user: { login: 'SantiMA10' } },
-				sender: { login: 'pepe' },
-			};
+			payload = new PullRequestPayloadBuilder()
+				.with({
+					action: 'opened',
+					repository: {
+						full_name: 'streamdevs/webhook',
+						html_url: 'https://github.com/streamdevs/webhook',
+					},
+					pull_request: { user: { login: 'SantiMA10' } },
+					sender: { login: 'pepe' },
+				})
+				.getInstance();
 		});
 
 		it("returns 'twitchChat.notified' === false if something goes wrong with TwitchChat", async () => {
